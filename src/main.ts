ad8ee3a0c443835b97d017630ab29f64b0570803
MrCroatia <petar.vlcek@yahoo.com>
2025-03-20 14:57:36 +0100

    speed opt

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,17 @@ import store from './stores'
 const app = createApp(App)
 app.use(store)
 app.use(router)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
 app.mount('#app')
